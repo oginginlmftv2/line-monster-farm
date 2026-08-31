3a89f71666b34eef68412a0b16a83e59322883e5
ogingintv <hounddogs0803@gmail.com>
2026-08-31 15:48:20 +0900

    CMS publish 2026-08-31 15:48:20

--- monsters-data.js
+++ monsters-data.js
@@ -354,4 +354,8 @@ const monstersData = [
   { name: 'ヤオビクニ', aura: '赤', limited: true, limitedLabel: 'イベント限定', gwImg: null, localImg: '2016.jpg', mon: '幻霊', mainMon: 'ウンディーネ', subMon: 'モッチー' },
   { name: 'エコスライム', aura: '緑', limited: true, limitedLabel: 'イベント限定', gwImg: null, localImg: '1921.jpg', mon: '幻霊', mainMon: 'ゲル', subMon: 'プラント' },
   { name: 'ルートヴィッヒ', aura: '黄', limited: true, limitedLabel: 'サマー限定', gwImg: null, localImg: '2580.jpg', mon: '怪物', mainMon: 'カワズモー', subMon: 'ノーブル' },
+  { name: 'ワルキューレ', aura: '白', limited: false, limitedLabel: '', gwImg: null, localImg: '3290.jpg', mon: '無機', mainMon: 'イルミネ', subMon: 'ロード種' },
+  { name: 'ラクシャーガ', aura: '黄', limited: false, limitedLabel: '', gwImg: null, localImg: null, mon: '無機', mainMon: 'ヘンガー', subMon: 'キジン' },
+  { name: 'センリ', aura: '白', limited: false, limitedLabel: '', gwImg: null, localImg: '2613.jpg', mon: '獣族', mainMon: 'キュービ', subMon: 'ニャー' },
+  { name: 'ベタピン', aura: '白', limited: true, limitedLabel: 'イベント限定', gwImg: null, localImg: '1553.jpg', mon: '魔族', mainMon: 'スエゾー', subMon: 'レアモン' },
 ];
